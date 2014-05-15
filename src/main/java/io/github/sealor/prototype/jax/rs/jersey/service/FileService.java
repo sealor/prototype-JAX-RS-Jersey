72dd10f51a6c215f8dbd31dda03aa2d69af1217d
Stefan Richter <jdevsr3@googlemail.com>
2014-05-15 12:39:16 +0200

    FileService should not expect octet streams

--- src/main/java/io/github/sealor/prototype/jax/rs/jersey/service/FileService.java
+++ src/main/java/io/github/sealor/prototype/jax/rs/jersey/service/FileService.java
@@ -10,7 +10,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -33,8 +32,8 @@ public class FileService {
 
 	@GET
 	@Path("download/{filename}")
-	@Produces(MediaType.APPLICATION_OCTET_STREAM)
 	public Response download(@PathParam("filename") String filename) {
-		return Response.ok().entity(this.fileStorage.get(filename)).build();
+		byte[] content = this.fileStorage.get(filename);
+		return Response.ok().type(MediaType.APPLICATION_OCTET_STREAM_TYPE).entity(content).build();
 	}
 }
